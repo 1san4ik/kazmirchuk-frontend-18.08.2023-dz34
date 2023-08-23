48d94d9c3d7820ad526ffa9197cb5a70e92e6e64
1san4ik <1san4ik@gmail.com>
2023-08-24 01:42:35 +0300

    Homework 34

--- product.js
+++ product.js
@@ -10,42 +10,42 @@ const productAroma = [
   {
     name: 'Ароматизатор 1',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ароматизатор 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/aroma/aroma-1.jpg',
     price: 70,
   },
   {
     name: 'Ароматизатор 2',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ароматизатор 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/aroma/aroma-2.jpg',
     price: 180,
   },
   {
     name: 'Ароматизатор 3',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ароматизатор 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/aroma/aroma-3.jpg',
     price: 360,
   },
   {
     name: 'Ароматизатор 4',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ароматизатор 4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/aroma/aroma-4.jpg',
     price: 65,
   },
   {
     name: 'Ароматизатор 5',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ароматизатор 5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/aroma/aroma-5.jpg',
     price: 400,
   },
   {
     name: 'Ароматизатор 6',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ароматизатор 6 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/aroma/aroma-6.jpg',
     price: 200,
   },
@@ -55,35 +55,35 @@ const productСovers = [
   {
     name: 'чохол салона 1',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'чохол салона 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/chohol/chohol-1.jpg',
     price: 5500,
   },
   {
     name: 'чохол салона 2',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'чохол салона 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/chohol/chohol-2.jpg',
     price: 6000,
   },
   {
     name: 'чохол салона 3',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'чохол салона 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/chohol/chohol-3.jpg',
     price: 4300,
   },
   {
     name: 'чохол салона 4',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'чохол салона 4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/chohol/chohol-4.jpg',
     price: 3800,
   },
   {
     name: 'чохол салона 5',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'чохол салона 5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/chohol/chohol-5.jpg',
     price: 3500,
   },
@@ -92,28 +92,28 @@ const productСompresor = [
   {
     name: 'Компресор 1',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Компресор 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/kompressor/kompressor-125000.jpg',
     price: 2000,
   },
   {
     name: 'Компресор 2',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Компресор 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/kompressor/kompressor-237100.jpg',
     price: 1500,
   },
   {
     name: 'Компресор 3',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Компресор 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/kompressor/kompressor-90135.jpg',
     price: 4300,
   },
   {
     name: 'Компресор 4',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Компресор 4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/kompressor/kompressor-90170.jpg',
     price: 3400,
   },
@@ -122,42 +122,42 @@ const productKovpaky = [
   {
     name: 'Ковпаки SKS - модель 1',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ковпаки SKS - модель 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/kovpaky/313.jpg',
     price: 500,
   },
   {
     name: 'Ковпаки SKS - модель 2',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ковпаки SKS - модель 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/kovpaky/324.jpg',
     price: 850,
   },
   {
     name: 'Ковпаки SKS - модель 3',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ковпаки SKS - модель 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/kovpaky/328.jpg',
     price: 930,
   },
   {
     name: 'Ковпаки SKS - модель 4',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ковпаки SKS - модель 4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/kovpaky/335.jpg',
     price: 680,
   },
   {
     name: 'Ковпаки SKS - модель 5',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ковпаки SKS - модель 5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/kovpaky/337.jpg',
     price: 790,
   },
   {
     name: 'Ковпаки SKS - модель 6',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Ковпаки SKS - модель 6 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/kovpaky/340.jpg',
     price: 790,
   },
@@ -166,42 +166,42 @@ const productPodushky = [
   {
     name: 'Подушка автомобільна 1',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Подушка автомобільна 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/podushki/podushka-1.jpg',
     price: 500,
   },
   {
     name: 'Подушка автомобільна 2',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Подушка автомобільна 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/podushki/podushka-2.jpg',
     price: 850,
   },
   {
     name: 'Подушка автомобільна 3',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Подушка автомобільна 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/podushki/podushka-3.jpg',
     price: 930,
   },
   {
     name: 'Подушка автомобільна 4',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Подушка автомобільна 4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/podushki/podushka-4.jpg#',
     price: 680,
   },
   {
     name: 'Подушка автомобільна 5',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Подушка автомобільна 5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/podushki/podushka-5.jpg',
     price: 790,
   },
   {
     name: 'Подушка автомобільна 6',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
+      'Подушка автомобільна 6 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, necessitatibus tenetur maxime repellendus mollitia esse sequi est? Aspernatur modi voluptates debitis nulla, laborum corrupti, odio, pariatur nemo atque tempore quod.',
     img: './images/tovar/podushki/podushka-6.jpg',
     price: 790,
   },
--- script.js
+++ script.js
@@ -25,12 +25,19 @@ navLinks.forEach((link, index) => {
     allCategory[index].forEach((link) => {
       shortProduct(link)
     })
+    addBorderClickListeners()
   })
 })
+
+addBorderClickListeners()
 
 function shortProduct(link) {
   const borderDiv = document.createElement('div')
   borderDiv.classList.add('border')
+  borderDiv.setAttribute('data-imgsrc', link.img)
+  borderDiv.setAttribute('data-name', link.name)
+  borderDiv.setAttribute('data-description', link.description)
+  borderDiv.setAttribute('data-price', link.price)
   colCenter.appendChild(borderDiv)
 
   const wrapDiv = document.createElement('div')
@@ -55,7 +62,7 @@ function shortProduct(link) {
 
   const productTitleH3 = document.createElement('h3')
   productTitleH3.classList.add('product-title')
-  productTitleH3.innerHTML = link.name
+  productTitleH3.innerText = link.name
   productInfoDiv.appendChild(productTitleH3)
 
   const priceDiv = document.createElement('div')
@@ -64,9 +71,95 @@ function shortProduct(link) {
   priceDiv.innerHTML = `Ціна: ${link.price} &#8372;`
 }
 
-const borderProduct = document.querySelectorAll('.border')
-borderProduct.forEach((link) => {
-  link.addEventListener('click', (event) => {
-    colRight.style.display = 'block'
+function addBorderClickListeners() {
+  const borderProduct = document.querySelectorAll('.border')
+  borderProduct.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      colRight.style.display = 'block'
+      const imgSrc = link.getAttribute('data-imgsrc')
+      const productName = link.getAttribute('data-name')
+      const productDescription = link.getAttribute('data-description')
+      const productPrice = link.getAttribute('data-price')
+
+      detailedProduct(imgSrc, productName, productDescription, productPrice)
+    })
   })
-})
+}
+
+function detailedProduct(
+  imgSrc,
+  productName,
+  productDescription,
+  productPrice
+) {
+  colRight.innerText = ''
+
+  const productContent = document.createElement('div')
+  productContent.classList.add('productContent')
+  colRight.appendChild(productContent)
+
+  const imgGallery = document.createElement('div')
+  imgGallery.classList.add('imgGallery')
+  productContent.appendChild(imgGallery)
+
+  const imgProduct = document.createElement('img')
+  imgGallery.appendChild(imgProduct)
+  imgProduct.src = imgSrc
+
+  const productInfo = document.createElement('div')
+  productInfo.classList.add('productInfo')
+  productContent.appendChild(productInfo)
+
+  const nameProduct = document.createElement('div')
+  nameProduct.classList.add('nameProduct')
+  nameProduct.innerText = productName
+  productInfo.appendChild(nameProduct)
+
+  const descriptionProduct = document.createElement('div')
+  descriptionProduct.classList.add('descriptionProduct')
+  descriptionProduct.innerText = productDescription
+  productInfo.appendChild(descriptionProduct)
+
+  const price = document.createElement('div')
+  price.classList.add('product-price')
+  productInfo.appendChild(price)
+  price.innerHTML = `Ціна: ${productPrice} &#8372;`
+
+  const buttonBuy = document.createElement('button')
+  buttonBuy.classList.add('buy-button')
+  buttonBuy.type = 'button'
+  buttonBuy.innerText = 'Купити'
+  productInfo.appendChild(buttonBuy)
+
+  buttonBuy.addEventListener('click', () => {
+    const modalWindow = document.createElement('div')
+    modalWindow.classList.add('modal')
+    modalWindow.innerHTML = `Ви купили товар: ${productName}`
+    colCenter.appendChild(modalWindow)
+    setTimeout(() => {
+      modalWindow.remove()
+      resetCategorySelection()
+    }, 3000)
+    colRight.style.display = 'none'
+  })
+  colRight.appendChild(productContent)
+  addBorderClickListeners()
+}
+
+function resetCategorySelection() {
+  navLinks.forEach((link) => {
+    link.classList.remove('active')
+  })
+
+  while (colCenter.firstChild) {
+    colCenter.removeChild(colCenter.firstChild)
+  }
+
+  allCategory.forEach((category) => {
+    category.forEach((link) => {
+      shortProduct(link)
+    })
+  })
+
+  addBorderClickListeners()
+}
